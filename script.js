df67d38f39a78bc476c9617e9355cf42a3446274
srikanthnagudu2909 <nagudusrikanth@gmail.com>
2025-09-17 18:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+function details(){
+         const name=document.getElementById("name").value
+        const year=document.getElementById("year").value
+        const heading=document.getElementById("heading1")
+         console.log(name,year)
+          heading.innerHTML=`https://localhost:8080/?name=${name}&year${year}`
+        }
